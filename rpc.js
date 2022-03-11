e6dd50247b78159802481ce07a8c03eb83f2d1d9
Dasuper11 <wrjp11@gmail.com>
2022-03-11 13:22:50 -0800

    UI added!

--- rpc.js
+++ rpc.js
@@ -1,17 +1,61 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        gameRound(button.id);
+    })
+
+})
+
 function computerPlay(){
-num = Math.floor(Math.random()*3)%3;
-if (num == 0){
-    return "Rock";
-}
-else if  (num == 1) {
-    return "Paper";    
-}
-else{
-    return "Scissors";
-}
+    num = Math.floor(Math.random()*3)%3;
+    if (num == 0){
+        return "Rock";
+    }
+    else if  (num == 1) {
+        return "Paper";    
+    }
+    else{
+        return "Scissors";
+    }
+};
+let cCount = 0;
+let pCount= 0;
+const Announce = document.querySelector('#Announcment');
+const pScore = document.querySelector('#pScore');
+const cScore = document.querySelector('#cScore');
+function gameRound(p){
+    c = computerPlay();
+    //reset if someone has won last round
+    if(pCount == 5 || cCount == 5){
+        pCount = 0;
+        cCount = 0;
+    }
+    //Round Winner Logic
+    if (c==p){
+        Announce.textContent="its a tie! you both selected " + p;
+    }
+    else if ((p=="Rock" && c=="Scissors") || (p=="Scissors" && c=="Paper") || (p=="Paper" && c=="Rock")){
+        Announce.textContent="You win this round! " + p + " beats " + c + "!"; pCount++;
+    }
+    else{
+        Announce.textContent="You lose this round! " + c + " beats " + p + "!"; cCount++;
+    }
+    pScore.textContent=pCount;
+    cScore.textContent=cCount;
+    //Game Winner
+    if(pCount == 5){
+        Announce.textContent="thats 5 rounds won, grats on winning!";
+    }
+    if(cCount == 5){
+        Announce.textContent="thats 5 rounds lost, better luck next time!!";
+    }
 }
 
-function playerPlay(){
+
+
+
+
+/*function playerPlay(){
 a = window.prompt("Time to play! Please enter 'Rock' 'Paper' or 'Scissors'")
 p1= a.slice(0,1);
 p2= a.slice(1, a.length);
@@ -19,7 +63,8 @@ p1=p1.toUpperCase();
 p2=p2.toLowerCase();
 output = p1+p2;
 return output;
-}
+} */
+
 
 function game(){
 p = playerPlay();
@@ -34,3 +79,4 @@ else{
     console.log("You lose! " + c + " beats " + p + "!")
 }
 }
+
